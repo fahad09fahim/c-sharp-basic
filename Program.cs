4022f26fcf121b6367fc536102951591d89bb83e
fahad09fahim <anik.fahim07@gmail.com>
2025-02-07 01:02:07 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// basic c# 
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+         Random ran = new Random();
+        
+      int ranNum = ran.Next(1,10);
+      int num=0;
+      while(num!=ranNum){
+           num = int.Parse(Console.ReadLine());
+          if(num<ranNum){
+             Console.WriteLine("Take bigger");
+          }else  if(num>ranNum){
+                 Console.WriteLine("Take less");
+          }else{
+              Console.WriteLine("Matched Number is: "+ num);
+          }
+      }
+
+    }
+}
