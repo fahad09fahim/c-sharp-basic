7e1824f170abde3134983f9b7af2dc3c5d264254
Fahad Al Fahim <anik.fahim07@gmail.com>
2025-02-17 22:00:27 +0600

    ms module one complete

---
+++ Fundamental-with-Ms/Program.cs
@@ -0,0 +1,19 @@
+﻿//Console.WriteLine("Hello, World!");
+
+/*Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code");
+Console.WriteLine("Congratulations!");
+Console.Write("You wrote your first lines of code");
+
+Float Type    Precision
+----------------------------
+float         ~6-9 digits
+double        ~15-17 digits
+decimal        28-29 digits
+
+*/
+//----------------------------------module two--------------------/
+//Console.WriteLine(0.25F);
+
+
