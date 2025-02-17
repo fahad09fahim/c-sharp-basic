dd7ae1a1c24d3b1af21200423b3ad448af9804dd
Fahad Al Fahim <anik.fahim07@gmail.com>
2025-02-17 22:13:39 +0600

    useCase of datatype and var

--- Fundamental-with-Ms/Program.cs
+++ Fundamental-with-Ms/Program.cs
@@ -15,5 +15,16 @@ decimal        28-29 digits
 */
 //----------------------------------module two--------------------/
 //Console.WriteLine(0.25F);
+/*string firstName;
+firstName = "Bob";
+Console.WriteLine(firstName);
+*/
+// useCase of data-types and var keyword
+/*string name = "Bob";
+int num = 3;
+var temperature = 34.4;
 
-
+Console.Write("Hello, "+name+"!");
+Console.Write(" You have "+num+" messages in your inbox.");
+Console.Write(" The temperature is "+temperature+" celsius.");
+*/
