407ebcca8a29b0831d0bf154bc0ccc5ee88c0e30
Fahad Al Fahim <anik.fahim07@gmail.com>
2025-02-23 21:02:50 +0600

    logical expression basic

---
+++ Expression/Program.cs
@@ -0,0 +1,9 @@
+﻿
+//Method thats returns a boolean value
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow"));
+
+// Use the Logical Negation operator
+
+Console.WriteLine(!pangram.Contains("cow"));
