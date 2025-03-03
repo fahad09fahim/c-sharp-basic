fd18d9d60710f6863a5362c9047d6f2faf3ea802
Fahad Al Fahim <anik.fahim07@gmail.com>
2025-03-03 23:45:27 +0600

    added do while on the menu selecting

--- GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ GuidedProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64a1a36954de18cc0bc3117e83873c55eed15c65")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97770a93f8ccd2801eae1a5516dcd0cc16eab372")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
