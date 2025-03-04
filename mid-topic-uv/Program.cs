e1cc03d0c034fcf54f0cf84ee30612164038043d
Fahad Al Fahim <anik.fahim07@gmail.com>
2025-03-04 23:35:55 +0600

    function overridding

---
+++ mid-topic-uv/Program.cs
@@ -0,0 +1,51 @@
+﻿
+//without overridding
+/*using System;
+class A
+{
+    public void display()
+    {
+        Console.WriteLine("Function of class A");
+    }
+}
+class B : A
+{
+    public void display()
+    {
+        Console.WriteLine("Function of class B");
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        A obj = new B();
+        obj.display();
+
+    }
+}*/
+//with overridding
+using System;
+class A
+{
+    public virtual void display()
+    {
+        Console.WriteLine("Function of class A");
+    }
+}
+class B : A
+{
+    public override void display()
+    {
+        Console.WriteLine("Function of class B");
+    }
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        A obj = new B();
+        obj.display();
+
+    }
+}
