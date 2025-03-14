bbcd035b283f54b4cfd0c19126808e483e54c089
Fahad Al Fahim <anik.fahim07@gmail.com>
2025-03-14 23:49:09 +0600

    indexof method and last indexof method usecase

--- data-type/Program.cs
+++ data-type/Program.cs
@@ -160,38 +160,46 @@ Formatting currency and numbers depend on the end user's culture, a five charact
 
 // Challenge string interpolation
 
-string customerName = "Ms. Barros";
+// string customerName = "Ms. Barros";
 
-string currentProduct = "Magic Yield";
-int currentShares = 2975000;
-decimal currentReturn = 0.1275m;
-decimal currentProfit = 55000000.0m;
+// string currentProduct = "Magic Yield";
+// int currentShares = 2975000;
+// decimal currentReturn = 0.1275m;
+// decimal currentProfit = 55000000.0m;
 
-string newProduct = "Glorious Future";
-decimal newReturn = 0.13125m;
-decimal newProfit = 63000000.0m;
+// string newProduct = "Glorious Future";
+// decimal newReturn = 0.13125m;
+// decimal newProfit = 63000000.0m;
 
-// Your logic here
-Console.WriteLine($"Dear {customerName}");
-Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.");
-Console.WriteLine(" ");
-Console.WriteLine($"Currently, you own {currentShares:N0} shares at a return of {currentReturn:P2}.");
-Console.WriteLine(" ");
-Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:N2}.");
-Console.WriteLine(" ");
+// // Your logic here
+// Console.WriteLine($"Dear {customerName}");
+// Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.");
+// Console.WriteLine(" ");
+// Console.WriteLine($"Currently, you own {currentShares:N0} shares at a return of {currentReturn:P2}.");
+// Console.WriteLine(" ");
+// Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:N2}.");
+// Console.WriteLine(" ");
 
-Console.WriteLine("Here's a quick comparison:\n");
+// Console.WriteLine("Here's a quick comparison:\n");
 
-string comparisonMessage = "";
+// string comparisonMessage = "";
 
 
-comparisonMessage = currentProduct.PadRight(20);
-comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
-comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+// comparisonMessage = currentProduct.PadRight(20);
+// comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+// comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
 
-comparisonMessage += "\n";
-comparisonMessage += newProduct.PadRight(20);
-comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
-comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+// comparisonMessage += "\n";
+// comparisonMessage += newProduct.PadRight(20);
+// comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+// comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
 
-Console.WriteLine(comparisonMessage);
+// Console.WriteLine(comparisonMessage);
+
+
+string message = "hello there!";
+
+int first_h = message.IndexOf('h');
+int last_h = message.LastIndexOf('h');
+
+Console.WriteLine($"For the message: '{message}', the first 'h' is at position {first_h} and the last 'h' is at position {last_h}.");
--- data-type/obj/Debug/net8.0/data-type.AssemblyInfo.cs
+++ data-type/obj/Debug/net8.0/data-type.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("data-type")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9964067f792b9454cac437923682ec7f9e650cc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63c06de1fa9f1299ed01a0c3c0d1f8f2bf5b072a")]
 [assembly: System.Reflection.AssemblyProductAttribute("data-type")]
 [assembly: System.Reflection.AssemblyTitleAttribute("data-type")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
