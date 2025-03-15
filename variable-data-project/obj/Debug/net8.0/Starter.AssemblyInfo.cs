42dd6b5e3b6aa3c3cffcd4a4986ff0916c8676e2
Fahad Al Fahim <anik.fahim07@gmail.com>
2025-03-15 15:23:31 +0600

    searching functionality added

--- variable-data-project/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ variable-data-project/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7969304832d3033da27809298bf7410f303451fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ad586eca83cd2d95592f9c58eee11bee52a79ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
